17f959b017fdcacde893fac3b80e56fe0b8b12f9
OlegYap1 <legiggid@gmail.com>
2023-11-23 20:43:32 +0800

    Создан логин

--- app/public/registrate.php
+++ app/public/registrate.php
@@ -1,70 +1,74 @@
 
 <?php
-
-function validate(array $data)
-{
-    $errors = [];
-    if (isset($data['name']))
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+if ($requestMethod === 'POST') {
+    function validate(array $data)
     {
-        $name = $data['name'];
-        if (strlen($name) < 2) {
-            $errors['name'] = 'Имя должно содержать больше двух букв';
+        $errors = [];
+        if (isset($data['name']))
+        {
+            $name = $data['name'];
+            if (strlen($name) < 2) {
+                $errors['name'] = 'Имя должно содержать больше двух букв';
+            }
+        } else {
+            $errors['name'] = 'Введите имя';
         }
-    } else {
-        $errors['name'] = 'Введите имя';
+
+        if (isset($data['email']))
+        {
+            $email = $data['email'];
+            if (strlen($email) < 4) {
+                $errors['email'] = 'Электронная почта должна содержать больше 4 символов';
+            } elseif (!strpos($email,'@')) {
+                $errors['email'] = 'Некорректный формат почты';
+            }
+        } else {
+            $errors['email'] = 'Введите email';
+        }
+
+        if (isset($data['psw-repeat'])) {
+            $password = $data['psw'];
+            $passwordR = $data['psw-repeat'];
+            if ($password !== $passwordR) {
+                $errors['psw'] = 'Пароль должен содержать больше 6 символов';
+            }
+        } else {
+            $errors['psw'] = 'Введите пароль';
+        }
+
+        return $errors;
     }
 
-    if (isset($data['email']))
+    $errors = validate($_POST);
+
+
+    if (empty($errors))
     {
-        $email = $data['email'];
-        if (strlen($email) < 4) {
-            $errors['email'] = 'Электронная почта должна содержать больше 4 символов';
-        } elseif (!strpos($email,'@')) {
-            $errors['email'] = 'Некорректный формат почты';
-        }
-    } else {
-        $errors['email'] = 'Введите email';
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['psw'];
+
+
+        $pdo = new PDO("pgsql:host=db;dbname=postgres","dbuser","dbpwd");
+
+        $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        $stmt->execute(['name' => $name,  'email' => $email, 'password' => $password]);
+
+        $stmt = $pdo->prepare('SELECT * FROM users');
+        $stmt->execute();
+        $data= $stmt ->fetchAll();
+
+        print_r($data);
     }
 
-    if (isset($data['psw-repeat'])) {
-        $password = $data['psw'];
-        $passwordR = $data['psw-repeat'];
-        if ($password !== $passwordR) {
-            $errors['psw'] = 'Пароль должен содержать больше 6 символов';
-        }
-    } else {
-        $errors['psw'] = 'Введите пароль';
-    }
-
-    return $errors;
 }
 
-$errors = validate($_POST);
-
-
-if (empty($errors))
-{
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['psw'];
-
-
-    $pdo = new PDO("pgsql:host=db;dbname=postgres","dbuser","dbpwd");
-
-    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-    $stmt->execute(['name' => $name,  'email' => $email, 'password' => $password]);
-
-    $stmt = $pdo->prepare('SELECT * FROM users');
-    $stmt->execute();
-    $data= $stmt ->fetchAll();
-
-    print_r($data);
-}
 ?>
 
 
 
-<form action="register_handler.php" method="POST" >
+<form action="login.php" method="POST" >
     <div class="container">
         <h1>Register</h1>
         <p>Please fill in this form to create an account.</p>
