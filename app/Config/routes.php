3c15ebb340214f9976b9fc5b3cd7483813ee942b
OlegYap1 <legiggid@gmail.com>
2024-02-05 22:01:18 +0800

    Add Config: dependencies, routes

---
+++ app/Config/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Controller\CartController;
+use Controller\MainController;
+use Controller\OrderController;
+use Controller\UserController;
+use Request\AddProductRequest;
+use Request\LoginRequest;
+use Request\OrderRequest;
+use Request\RegistrateRequest;
+
+function  routes(App $app, $container): App
+{
+    $app->setContainer($container);
+
+    $app->get('/registrate', UserController::class, 'getRegistrateForm');
+    $app->post('/registrate', UserController::class, 'postRegistrate', RegistrateRequest::class);
+
+    $app->get('/login', UserController::class, 'getLogin');
+    $app->post('/login', UserController::class, 'postLogin', LoginRequest::class);
+
+
+    $app->get('/main', MainController::class, 'getMainPage');
+    $app->post('/main', CartController::class, 'postAddProduct', AddProductRequest::class);
+
+    $app->get('/cart', CartController::class, 'getCartPage');
+
+    $app->get('/order', OrderController::class, 'getOrderForm');
+    $app->post('/order', OrderController::class, 'postOrder', OrderRequest::class);
+
+    $app->get('/order-product', OrderController::class, 'getOrder');
+
+    return $app;
+}
--- app/public/index.php
+++ app/public/index.php
@@ -15,52 +15,11 @@ use Service\OrderService;
 require_once '../Autoloader.php';
 Autoloader::registrate(dirname(__DIR__));
 
-$container = new Container();
-$container->set(OrderController::class, function (Container $container){
-    $orderService = new OrderService();
-    $authenticationSerivice = $container->get(AuthenticationInterface::class);
-    return new OrderController($orderService,$authenticationSerivice);
-});
+require_once '../Config/dependencies.php';
+require_once '../Config/routes.php';
 
-$container->set(CartController::class, function (Container $container){
-    $authenticationService = $container->get(AuthenticationInterface::class);
-    return new CartController($authenticationService);
-});
+$container = depend(new Container());
 
-$container->set(MainController::class, function (Container $container) {
-    $authenticationService = $container->get(AuthenticationInterface::class);
-
-    return new MainController($authenticationService);
-});
-
-$container->set(UserController::class, function (Container $container) {
-    $authenticationService = $container->get(AuthenticationInterface::class);
-
-    return new UserController($authenticationService);
-});
-
-$container->set(AuthenticationInterface::class, function () {
-    return new SessionAuthenticationService(); // Here we can change on Cookie Authentication Service
-});
-
-$app = new App();
-$app->setContainer($container);
-
-$app->get('/registrate', UserController::class, 'getRegistrateForm');
-$app->post('/registrate', UserController::class, 'postRegistrate', RegistrateRequest::class);
-
-$app->get('/login', UserController::class, 'getLogin');
-$app->post('/login', UserController::class, 'postLogin', LoginRequest::class);
-
-
-$app->get('/main', MainController::class, 'getMainPage');
-$app->post('/main', CartController::class, 'postAddProduct', AddProductRequest::class);
-
-$app->get('/cart', CartController::class, 'getCartPage');
-
-$app->get('/order', OrderController::class, 'getOrderForm');
-$app->post('/order', OrderController::class, 'postOrder', OrderRequest::class);
-
-$app->get('/order-product', OrderController::class, 'getOrder');
+$app = routes(new App(), $container);
 
 $app->run();
